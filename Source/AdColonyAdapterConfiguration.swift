ecd369fd1f17c74aba1fec79d5cbd29b143ee283
Daniel Barros <daniel.barros@chartboost.com>
2022-12-21 15:57:29 +0100

    Make configuration class compatible with Obj-C

--- Source/AdColonyAdapterConfiguration.swift
+++ Source/AdColonyAdapterConfiguration.swift
@@ -6,31 +6,27 @@
 import AdColony
 
 /// A list of externally configurable properties pertaining to the partner SDK that can be retrieved and set by publishers.
-public class AdColonyAdapterConfiguration {
+@objc public class AdColonyAdapterConfiguration: NSObject {
 
     /// Flag that can optionally be set to enable the partner's test mode.
     /// Disabled by default.
-    public static var testMode: Bool = false {
+    @objc public static var testMode: Bool = false {
         didSet {
             let options = AdColonyAdapter.options
             options.testMode = testMode
             AdColony.setAppOptions(options)
-
-            print("The AdColony SDK's test mode is \(testMode ? "enabled" : "disabled").")
+            print("AdColony SDK test mode set to \(testMode)")
         }
     }
     
     /// Flag that can optionally be set to enable the partner's verbose logging.
     /// Disabled by default.
-    public static var verboseLogging: Bool = false {
+    @objc public static var verboseLogging: Bool = false {
         didSet {
             let options = AdColonyAdapter.options
             options.disableLogging = !verboseLogging
             AdColony.setAppOptions(options)
-
-            print("The AdColony SDK's verbose logging is \(verboseLogging ? "enabled" : "disabled").")
+            print("AdColony SDK verbose logging set to \(verboseLogging)")
         }
     }
-    
-    /// Append any other properties that publishers can configure.
 }

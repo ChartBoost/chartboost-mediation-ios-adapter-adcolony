4795801d564910352c7734e4ce0dd7d0d87352c7
Daniel Barros <daniel.barros@chartboost.com>
2023-08-18 16:12:26 -0700

    backward comptible change

--- Source/AdColonyAdapter.swift
+++ Source/AdColonyAdapter.swift
@@ -146,6 +146,7 @@ final class AdColonyAdapter: NSObject, PartnerAdapter {
         guard request.partnerPlacement == zone.identifier else {
             throw error(.loadFailureAborted, description: "Placement is different from the zone identifier.")
         }
+        print("hello")
         switch request.format {
         case .interstitial, .rewarded:
             return AdColonyAdapterFullscreenAd(adapter: self, request: request, delegate: delegate, zone: zone)
